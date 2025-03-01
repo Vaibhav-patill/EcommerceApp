9545f4c2fd9a35b5035b96f709df961aec6cd460
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-03-01 23:38:16 +0530

    completed products section and added relaetd products section

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products,currency } = useContext(ShopContext);
+  const { products, currency,addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -54,18 +56,74 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3 5" />
             <p className="pl-2">(122)</p>
           </div>
-          <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
-          <p className="mt-5 text-gray-700 md:w-4/5">{productData.description}</p>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-700 md:w-4/5">
+            {productData.description}
+          </p>
           <div className="flex flex-col gap-4 my-8">
             <p>Select Size</p>
             <div className="flex gap-2">
-              {productData.sizes.map((item,index)=>(
-                <button className={`border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border-1 py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
               ))}
             </div>
           </div>
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white border  px-4 py-2 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product</p>
+            <p>Cash on Delivery is available on this product</p>
+            <p>Easy return exchange policy within 7 days</p>
+          </div>
         </div>
       </div>
+      {/* description and review section */}
+      <div className="mt-20">
+        <div className="flex ">
+          <p className="border border-gray-300 px-5 py-3 text-sm">
+            Description
+          </p>
+          <p className="border border-gray-300 px-5 py-3 text-sm">
+            Reviews (122)
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 border border-gray-300 text-sm text-gray-500 px-6 py-6">
+          <p>
+            An e-commerce website is an online platform that facilitates the
+            buying and selling of products or services over the internet. It
+            serves as a virtual marketplace where businesses and individuals can
+            showcase their products, interact with customers, and conduct
+            transactions without the need for a physical presence. E-commerce
+            websites have gained immense popularity due to their convenience,
+            accessibility, and the global reach they offer.
+          </p>
+          <p>
+            E-commerce websites typically display products or services along
+            with detailed descriptions, images, prices, and any available
+            variations (e.g., sizes, colors). Each product usually has its own
+            dedicated page with relevant information.
+          </p>
+        </div>
+      </div>
+
+      {/* display related products */}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory }
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
