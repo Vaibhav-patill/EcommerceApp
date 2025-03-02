2b49b099939091bc87b9829aff7bb4757ec2c2c4
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-03-02 20:58:39 +0530

    added functionality for increasing count in cart

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,47 +1,68 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
-export const ShopContext=createContext();
+export const ShopContext = createContext();
 
-const ShopContextProvider=(props)=>{
+const ShopContextProvider = (props) => {
+  const currency = "₹";
+  const deliver_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
 
-    const currency='₹'
-    const deliver_fee=10;
-    const [search,setSearch]=useState('');
-    const [showSearch,setShowSearch]=useState(false);
-    const [cartItems,setCartItems]=useState({});
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
 
-    const addToCart = async (itemId,size)=>{
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
 
-        let cartData=structuredClone(cartItems);
-        if(cartData[itemId]){
-            if(cartData[itemId][size]){
-                cartData[itemId][size]+=1;
-            }else{
-                cartData[itemId][size]=1;
+  const getCartCount = () => {
+    let count = 0;
+    for (let item in cartItems) {
+      for (const size in cartItems[item]) {
+        try {
+            if(cartItems[item][size] > 0){
+                count += cartItems[item][size];
             }
-        }else{
-            cartData[itemId]={};
-            cartData[itemId][size]=1;
-        }
-        setCartItems(cartData);
+        } catch (error) {}
+      }
     }
+    return count;
+  };
 
-    useEffect(()=>{
-        console.log(cartItems)
-    },[cartItems])
+  useEffect(() => {}, [cartItems]);
 
+  const value = {
+    products,
+    currency,
+    deliver_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount
+  };
 
-    const value={
-        products,currency,deliver_fee,
-        search,setSearch,showSearch,setShowSearch,cartItems,addToCart
-    }
-
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
